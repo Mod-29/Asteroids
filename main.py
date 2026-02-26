767c817ff61b6aa3bee778bceb87283cd7bffbe1
Mod-29 <dominickdejesus29@gmail.com>
2026-02-26 15:56:19 -0500

    created game clock and used .tick(60) to restrict the code to redraw the game screen 60 tps instead of redrawing as fast as the cpu is running

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         log_state()
@@ -20,6 +22,8 @@ def main():
         pass
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+        
 
 
 
