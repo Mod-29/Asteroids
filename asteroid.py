e48f6cb497db3adea3bc3ee4791df36c468cf426
Mod-29 <dominickdejesus29@gmail.com>
2026-02-28 12:08:10 -0500

    created function to split med + large asteroids when shot into smaller asteroids

--- asteroid.py
+++ asteroid.py
@@ -1,6 +1,8 @@
 import pygame
+import random
+from logger import *
 from circleshape import CircleShape
-from constants import LINE_WIDTH
+from constants import *
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
@@ -11,3 +13,20 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += self.velocity * dt
+
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        else:
+            log_event("asteroid_split")
+            new_angle = random.uniform(20, 50)
+            new_vector1 = self.velocity.rotate(new_angle)
+            new_vector2 = self.velocity.rotate(-new_angle)
+            new_asteroids_radius = self.radius - ASTEROID_MIN_RADIUS
+            asteroid1 = Asteroid(self.position.x, self.position.y, new_asteroids_radius)
+            asteroid2 = Asteroid(self.position.x, self.position.y, new_asteroids_radius)
+            asteroid1.velocity = new_vector1 * 1.2
+            asteroid2.velocity = new_vector2 * 1.2
+            
+
